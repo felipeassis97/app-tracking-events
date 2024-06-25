51c3beb065c404b019ff89f8facbbcf532deb6af
felipeassis97 <felipeassis97@gmail.com>
2024-06-25 14:49:19 +1000

    doc: update functions docs

--- lib/core/i_tracking_events.dart
+++ lib/core/i_tracking_events.dart
@@ -2,25 +2,26 @@ import 'package:app_tracking_events/core/i_tracking_analytics.dart';
 import 'package:app_tracking_events/core/i_tracking_crashlytics.dart';
 
 /// You should use some of the available providers
-/// You can create an instance like this:
-///```dart
-/// final ISearsLogger logger = FirebaseMethod(firebaseOptions: ...);
-/// ```
-/// ___Obs.:__ In the example above, the __FirebaseMethod__ is being displayed, but you can use any method available in the library_
+/// - You can create an instance like this:
+///   ```dart
+///     final ITrackingEvents logger = FirebaseMethod(firebaseOptions: ...);
+///   ```
 ///
-/// You can create an instance using Get It or another dependency managerlike this:
-/// ```dart
-///   getIt.registerLazySingleton<ISearsLogger>(FirebaseMethod(ferebaseOptions: ...));
-/// ```
-/// - Functions available
-///   - Analytics
+/// > ___Obs.:__ In the example above, the __FirebaseMethod__ is being displayed, but you can use any method available in the library_
+///
+/// - You can create an instance using Get It or another dependency managerlike this:
+///   ```dart
+///     getIt.registerLazySingleton<ITrackingEvents>(FirebaseMethod(ferebaseOptions: ...));
+///   ```
+/// - #### Functions available
+///   - __Analytics__
 ///     * logEvents
 ///     * logSignin
 ///     * logSignout
 ///     * logSignUp
 ///     * logAppOpen.
 ///     * resetAllDeviceData
-///   - Crashlytics
+///   - __Crashlytics__
 ///     * recordError
 ///     * recordNetworkError
 abstract class ITrackingEvents {
