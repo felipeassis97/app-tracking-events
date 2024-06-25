6981102e8d712219b720627d0b860c0a47f97bd5
felipeassis97 <felipeassis97@gmail.com>
2024-06-26 08:36:50 +1000

    fix/credentials

--- lib/methods/firebase/credentials/firebase_credentials.dart
+++ lib/methods/firebase/credentials/firebase_credentials.dart
@@ -16,19 +16,3 @@ final class FirebaseCredentials {
     required this.iosBundleId,
   });
 }
-
-//Android
-  //apiKey: AIzaSyCAlQAUwDfAy25gD17gf3lsmE6ZHFKyaEU
-  //appId: 1:851793481185:android:a33c885121939424f196a0
-  //projectId: sears-dev-32a1d
-  //storageBucket: sears-dev-32a1d.appspot.com
-  // messagingSenderId: 851793481185
-
-//IOS
-  // apiKey: AIzaSyA_vvrsWMZsk_ftSJeYwhJr4ncX5Crp0ic
-  // appId: 1:851793481185:ios:c3cecc07d5686a0bf196a0
-  // messagingSenderId: 851793481185
-  // iosBundleId: com.evertec.wallet.sears
-  // projectId: sears-dev-32a1d
-  // storageBucket: sears-dev-32a1d.appspot.com
-
